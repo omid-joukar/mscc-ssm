06954e3e01a6c3ca3f222fd373cbc50fa718b168
omid-joukar <joukaromid6@gmail.com>
2021-06-10 09:04:41 +0200

    added transition configuration with test.

---
+++ src/test/java/omid/springframework/msscssm/config/StateMachineConfigTest.java
@@ -0,0 +1,33 @@
+package omid.springframework.msscssm.config;
+
+import omid.springframework.msscssm.domain.PaymentEvent;
+import omid.springframework.msscssm.domain.PaymentState;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class StateMachineConfigTest {
+    @Autowired
+    StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+    @Test
+    void testNewStateMachine(){
+        StateMachine<PaymentState, PaymentEvent> sm = factory.getStateMachine(UUID.randomUUID());
+        sm.start();
+        System.out.println(sm.getState().toString());
+        sm.sendEvent(PaymentEvent.PRE_AUTHORIZED);
+        System.out.println(sm.getState().toString());
+        sm.sendEvent(PaymentEvent.PRE_AUTH_PROVED);
+        System.out.println(sm.getState().toString());
+        sm.sendEvent(PaymentEvent.PRE_AUTH_DECLINED);
+        System.out.println(sm.getState().toString());
+
+
+    }
+}
