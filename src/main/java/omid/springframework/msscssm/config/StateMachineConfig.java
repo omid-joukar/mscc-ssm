2588fbfd4eac18c5e2a569c83cb01dfa9d1de314
omid-joukar <joukaromid6@gmail.com>
2021-06-10 08:42:25 +0200

    added state machine configuration.

---
+++ src/main/java/omid/springframework/msscssm/config/StateMachineConfig.java
@@ -0,0 +1,26 @@
+package omid.springframework.msscssm.config;
+
+import lombok.extern.slf4j.Slf4j;
+import omid.springframework.msscssm.domain.PaymentEvent;
+import omid.springframework.msscssm.domain.PaymentState;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
+
+@Slf4j
+@Configuration
+@EnableStateMachineFactory
+public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+    @Override
+    public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
+        states.withStates()
+                .initial(PaymentState.NEW)
+                .states(EnumSet.allOf(PaymentState.class))
+                .end(PaymentState.AUTH)
+                .end(PaymentState.PRE_AUTH_ERROR)
+                .end(PaymentState.AUTH_ERROR);
+    }
+}
